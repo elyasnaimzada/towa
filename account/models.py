7feeef1fc4f0e805ac38a6249afce69e45c8bf2a
elyasnaimzada <63570051+elyasnaimzada@users.noreply.github.com>
2020-05-13 21:59:53 +1000

    Update models.py

--- account/models.py
+++ account/models.py
@@ -8,15 +8,15 @@ class Jobs(models.Model):
     invoiceno = models.IntegerField()
     transtype = models.CharField(max_length=200)
     date = models.CharField(max_length=200)
-    ticket = models.IntegerField()
-    externalno = models.CharField(max_length=200)
-    cctname = models.CharField(max_length=200)
+    ticket = models.IntegerField(blank=True)
+    externalno = models.CharField(max_length=200, blank=True)
+    cctname = models.CharField(max_length=200, blank=True)
     location = models.CharField(max_length=200, blank=True)
-    sordescription = models.TextField(max_length=None)
-    qty = models.IntegerField()
-    rate = models.FloatField()
-    gst = models.FloatField()
-    total = models.FloatField()
+    sordescription = models.TextField(max_length=None,blank=True )
+    qty = models.IntegerField(blank=True)
+    rate = models.FloatField(blank=True)
+    gst = models.FloatField(blank=True)
+    total = models.FloatField(blank=True)
 
     def __str__(self):
         return self.location
